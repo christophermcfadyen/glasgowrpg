32614ae0aab98f4fd292e23140b21487636aa5c9
Chris <2030808m@student.gla.ac.uk>
2018-03-20 18:01:38 +0000

    fix attempt 4

---
+++ oldDirectory/rpg/views.py
@@ -0,0 +1,43 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponseRedirect, HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
+from datetime import datetime
+
+
+def home(request):
+    
+    
+    context_dict = {}
+    response = render(request, 'rpg/home.html', context=context_dict)
+    return response
+
+
+def about(request):
+
+    context_dict =  {'boldmessage': "about"}
+    return render(request, 'rpg/about.html', context=context_dict)
+    
+def help(request):
+
+    context_dict =  {'boldmessage': "help"}
+    return render(request, 'rpg/help.html', context=context_dict)
+    
+def login(request):
+
+    context_dict =  {'boldmessage': "login"}
+    return render(request, 'rpg/login.html', context=context_dict)
+    
+def register(request):
+
+    context_dict =  {'boldmessage': "register"}
+    return render(request, 'rpg/register.html', context=context_dict)
+    
+def play(request):
+    context_dict = {'boldmessage': "play"}
+    return render(request, 'rpg/play.html', context=context_dict)
+
+
+
