9fbc6289a04bd8f93570e9b4f19e853a79f704f0
christophermcfadyen email = 2030808m@student.gla.ac.uk <2030808m@AD.DCS.GLA.AC.UK>
2018-03-21 19:35:00 +0000

    Added to views/urls 5th? attempt

--- glasgowrpg/rpg/urls.py
+++ glasgowrpg/rpg/urls.py
@@ -1,5 +1,8 @@
 from django.conf.urls import url
 from rpg import views
+
+app_name = "rpg"
+
 urlpatterns = [
     url(r'^$', views.home, name='home'),
     url(r'about/$', views.about, name='about'),
@@ -9,5 +12,3 @@ urlpatterns = [
     url(r'play/$', views.play, name='play'),
     url(r'stats/$', views.stats, name='stats'),
     url(r'userprofile/$', views.userProfile, name='userProfile'),]
-
-
