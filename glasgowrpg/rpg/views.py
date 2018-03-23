a423ddf560c028e995b8209890a605903158ef2e
christophermcfadyen email = 2030808m@student.gla.ac.uk <2030808m@AD.DCS.GLA.AC.UK>
2018-03-23 11:24:12 +0000

    Euan's Updated view

--- glasgowrpg/rpg/views.py
+++ glasgowrpg/rpg/views.py
@@ -5,33 +5,25 @@ from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from datetime import datetime
-from django.urls import reverse
-from rpg.forms import UserForm, UserProfileForm #added euan's part
+from django.core.urlresolvers import reverse
+from rpg.forms import UserForm, UserProfileForm
 
 
 def home(request):
-
-
-    context_dict = {}
+    user_list = UserProfile.objects.order_by('-academic_score')[:3]
+    context_dict = {'users', user_list}
     response = render(request, 'rpg/home.html', context=context_dict)
     return response
 
 
 def about(request):
-
     context_dict =  {'boldmessage': "about"}
     return render(request, 'rpg/about.html', context=context_dict)
-
+    
 def help(request):
-
     context_dict =  {'boldmessage': "help"}
     return render(request, 'rpg/help.html', context=context_dict)
-
-#def login(request):
-
-#    context_dict =  {'boldmessage': "login"}
-#    return render(request, 'rpg/login.html', context=context_dict)
-
+    
 def user_login(request):
     if request.method == 'POST':
         username = request.POST.get('username')
@@ -49,14 +41,7 @@ def user_login(request):
             return HttpResponse("Invalid login details supplied.")
     else:
         return render(request, 'rpg/login.html', {})
-
-
-
-#def register(request):
-
-#    context_dict =  {'boldmessage': "register"}
-#    return render(request, 'rpg/register.html', context=context_dict)
-
+    
 def register(request):
     registered = False
 
@@ -84,27 +69,21 @@ def register(request):
                    'profile_form':profile_form,
                    'registered':registered})
 
+def stats(request):
+    print("TBA")
 
-
+'''
+user should be logged in to play... otherwise you are not restricting
+any functionality of the site to the users?
+'''
+@login_required   
 def play(request):
     context_dict = {'boldmessage': "play"}
     return render(request, 'rpg/play.html', context=context_dict)
-
-def userprofile(request):
-    context_dict = {'boldmessage': "userprofile"}
-    return render(request, 'rpg/userprofile.html', context=context_dict)
-
-def stats(request): #user has to be logged in to view this
-    context_dict = {'boldmessage': "stats"}
-    return render(request, 'rpg/stats.html', context=context_dict)
-
 
 @login_required
 def user_logout(request):
     logout(request)
     return HttpResponseRedirect(reverse('home'))
-
-#added Euan's part, changes in register and login and logout
-
 
 
