c197f2c98d4928f8beb9c9513e3ffb31538c62d1
Chris <2030808m@student.gla.ac.uk>
2018-03-23 14:51:52 +0000

    Tidy repo

--- oldDirectory/rpg/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-
-class CategoryAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug':('name',)}
-    
--- oldDirectory/rpg/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-from __future__ import unicode_literals
-
-
-class RPGConfig(AppConfig):
-    name = 'rpg'
