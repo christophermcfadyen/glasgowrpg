186da38a010d2c380f51890bd5818510152e4bf0
Chris <2030808m@student.gla.ac.uk>
2018-03-24 23:10:53 +0000

    Added admin interface

--- glasgowrpg/glasgowRPG/settings.py
+++ glasgowrpg/glasgowRPG/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
-#STATIC_DIR = os.path.join(BASE_DIR, 'static')
 MEDIA_DIR =os.path.join(BASE_DIR, 'media')
 
 # MEDIA
@@ -128,13 +127,19 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT= ''
 STATIC_URL = '/static/'
-STATIC_ROOT= os.path.join(BASE_DIR,'css/')
 
-STATICFILES_DIRS = (
-  os.path.join(BASE_DIR, 'static/'),
+
+STATICFILES_DIRS = (STATIC_PATH,)
+
+#Login Redirect
+LOGIN_URL = '/rpg/login/'
+
+ADMINS = (
+    #,
 )
 
-
-LOGIN_URL = '/rpg/login/'
+MANAGERS = ADMINS
 
