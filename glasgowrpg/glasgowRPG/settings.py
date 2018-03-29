8d91455f69ce11090fb3cc5845e06a5378efe017
christophermcfadyen email = 2030808m@student.gla.ac.uk <2030808m@AD.DCS.GLA.AC.UK>
2018-03-29 13:54:45 +0100

    Accidental Deletion

--- glasgowrpg/glasgowRPG/settings.py
+++ glasgowrpg/glasgowRPG/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = '!f$w!m&l+=r_(5v0c9j6la=wqazk3vuz@*)3wo_rfohjberf^j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['2030808m.pythonanywhere.com','nickhan123.pythonanywhere.com']
+ALLOWED_HOSTS = ['2030808m.pythonanywhere.com','nickhan123.pythonanywhere.com','127.0.0.1']
 
 
 # Application definition
